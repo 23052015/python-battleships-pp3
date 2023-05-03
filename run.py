869b1420aacb47683800492723c9ebd8fa0ef5be
23052015 <stani.ste13@gmail.com>
2023-05-03 19:26:47 +0000

    add basic structure menu layout

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random 
+import time
+
+
+
+
+
+def menu():
+    """
+    prints the main menu and shows options to choose
+    """
+    while True
+    print("1.New Game")
+    print("Instructions")
+    print("Credits")
+
+    choice = input("Choose option")
+    if choice = '1':
+        def
+    elif choice = '2':
+        def
+    elif choice '3':
+        def
